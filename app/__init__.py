13a611f9a208c1323da6ebb281fbc352fb1c4070
Kate <you@example.com>
2020-09-27 17:29:02 +0300

    bootstrap initialized

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,4 @@
-from flask import Flask
-# from app import app
-from .config import DevConfig
+from flask_sqlalchemy import SQLAlchemy
 
-# Initializing application
-app = Flask(__name__)
-
-# Setting up configuration
-app.config.from_object(DevConfig)
-
-from app import views
+bootstrap = Bootstrap()
+db = SQLAlchemy()
