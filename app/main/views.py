9260ec3ecef43275249cd1a2635bc952a4ce3807
Kate <you@example.com>
2020-09-27 14:16:09 +0300

    title in view file added

--- app/main/views.py
+++ app/main/views.py
@@ -8,4 +8,7 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+
+     title = 'Home - Welcome to The best Online blog center'
+
     return render_template('index.html')
