f730420ea9707b3ba9f11f6c9ecb88b837db57fb
Kate <you@example.com>
2020-09-27 17:42:19 +0300

    relationship created

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
+# from flask_bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap
+# from config import config_options
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
--- config.py
+++ config.py
@@ -2,7 +2,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    # SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kate:Kate@localhost/blog'
 
 
