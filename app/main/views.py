6416a370c8338120706755875956e0d7fd976851
Kate <you@example.com>
2020-09-27 16:47:31 +0300

    title

--- app/main/views.py
+++ app/main/views.py
@@ -13,7 +13,7 @@ def index():
     quotes = get_quotes(quote)
     page = request.args.get('page',1, type = int )
     blogs = Blog.query.order_by(Blog.posted.desc()).paginate(page = page, per_page = 3)
-        title = 'Home - Welcome to The best Movie Review Website Online'
+    title = 'Home - Welcome to The best Movie Review Website Online'
 
     return render_template('index.html', quote = quotes,blogs=blogs)
     # return render_template('index.html', title = title,popular = quotes)
