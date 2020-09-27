7c108f81db464b9c4259ab73d9034c709916cdf5
Kate <you@example.com>
2020-09-27 15:24:30 +0300

    qotes url

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+# from app import app
 from .config import DevConfig
 
 # Initializing application
--- app/main/views.py
+++ app/main/views.py
@@ -9,6 +9,11 @@ def index():
     View root page function that returns the index page and its data
     '''
 
-     title = 'Home - Welcome to The best Online blog center'
+    # Getting popular movie
+    quotes = get_quotes(quote)
+    page = request.args.get('page',1, type = int )
+    blogs = Blog.query.order_by(Blog.posted.desc()).paginate(page = page, per_page = 3)
+        title = 'Home - Welcome to The best Movie Review Website Online'
 
-    return render_template('index.html', title = title)
+    return render_template('index.html', quote = quotes,blogs=blogs)
+    # return render_template('index.html', title = title,popular = quotes)
