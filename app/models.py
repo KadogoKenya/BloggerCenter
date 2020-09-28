bdf7a870f6954a99c0eb25b403e493da83b3d679
Kate <you@example.com>
2020-09-28 04:48:04 +0300

    init file created

--- app/models.py
+++ app/models.py
@@ -20,3 +20,22 @@ class Role(db.Model):
 
     def __repr__(self):
         return f'User {self.name}'
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.name}'
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.name}'
