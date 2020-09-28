37e4ab6b8759d8c4404f4467e9343931e557e442
Kate <you@example.com>
2020-09-28 05:27:20 +0300

    models file updated

--- app/models.py
+++ app/models.py
@@ -1,41 +1,55 @@
 from . import db
+from app import login_manager
+from flask_login import UserMixin
+from datetime import datetime
 
-#...
 
-class User(db.Model):
+
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    username = db.Column(db.String(1000), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    # blogposts = db.relationship('Blog',backref='author', lazy='dynamic')
+    
+    @login_manager.user_loader
+    def get_user(user_id):
+        return User.query.get(user_id)
+
 
     def __repr__(self):
-        return f'User {self.username}'
+        return f'User{self.name}'
 
 
+class Blog(db.Model):
+    __tablename__ = 'blogposts'
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100),nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text,nullable=False)
+    # users_id = db.Column(db.Integer, db.ForeignKey('users.id'),nullable=False)
 
-class Role(db.Model):
-    __tablename__ = 'roles'
-
-    id = db.Column(db.Integer,primary_key = True)
-    name = db.Column(db.String(255))
 
     def __repr__(self):
-        return f'User {self.name}'
-
-class Post(db.Model):
-    __tablename__ = 'posts'
-
-    id = db.Column(db.Integer,primary_key = True)
-    name = db.Column(db.String(255))
-
-    def __repr__(self):
-        return f'User {self.name}'
+        return f"Post('{self.title}','{self.date_posted}')"
 
 
 class Comment(db.Model):
     __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(255), nullable=False)
+    dateposted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    post_id = db.Column(db.Integer, db.ForeignKey('blogposts.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
-    id = db.Column(db.Integer,primary_key = True)
-    name = db.Column(db.String(255))
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
 
     def __repr__(self):
-        return f'User {self.name}'
+        return f"Comment('{self.content}', '{self.dateposted}')"
